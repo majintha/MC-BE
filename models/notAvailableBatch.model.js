ad9b4b3c25cdd02b3e02f6da64ab2624cb986035
HP <majintha.c@gmail.com>
2021-05-19 12:07:32 +0530

    MSLG.h Done Backend

--- models/notAvailableBatch.model.js
+++ models/notAvailableBatch.model.js
@@ -3,6 +3,8 @@ const { Schema } = mongoose;
 
 const notAvailableBatchSchema = new mongoose.Schema({
     batchId: { type: Schema.Types.ObjectId, ref: 'batch'},
+    subject1: { type: String},
+    subject: { type: String},
     day: { type: String},
     startTime: { type: String},
     endTime: { type: String}
